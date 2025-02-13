b88ad5cd7568e2c968783cfefcda293f9c7a7339
Cam <8885122+Camayy@users.noreply.github.com>
2025-02-13 03:00:03 +0000

    added html reporter and updated readme

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,11 +1,18 @@
 import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
+    reporter: [
+        ['list'],  // Default console output
+        ['html', { outputFolder: 'test-results/html-report' }],
+        ['junit', { outputFile: 'test-results/results.xml' }]  
+      ],
   use: {
     baseURL: 'https://web-cdn.api.bbci.co.uk',
     extraHTTPHeaders: {
       'Content-Type': 'application/json',
       'Authorization': 'Bearer TOKEN' 
-    }
+    },
+    trace: 'on', 
+    screenshot: 'only-on-failure' 
   }
 });
--- tests/api.test.js
+++ tests/api.test.js
@@ -1,23 +1,24 @@
 import { test, expect } from '@playwright/test';
 
+const endpoint = "/wc-poll-data/container/sport-standings?urn=urn:bbc:sportsdata:rugby-union:tournament:six-nations";
+const partialEndpoint = "/wc-poll-data/container/sport-standings?urn=urn:bbc:sportsdata:rugby-union:tournament:"
+
 test.describe('API Tests - BBC Sport Standings', () => {
     test('GET request returns 200 and response time under 1000ms', async ({ request }) => {
         const startTime = Date.now();
-        const response = await request.get('/wc-poll-data/container/sport-standings?urn=urn:bbc:sportsdata:rugby-union:tournament:six-nations');
+        const response = await request.get(endpoint);
         const duration = Date.now() - startTime;
 
         expect(response.status()).toBe(200);
-        expect(duration).toBeLessThan(1000);
-
-        console.log(`Response Time: ${duration}ms`);
+        expect(duration).toBeLessThan(1000);;
     });
 
     test('GET request should have a valid id and 6 participants', async ({ request }) => {
-        const response = await request.get('/wc-poll-data/container/sport-standings?urn=urn:bbc:sportsdata:rugby-union:tournament:six-nations');
+        const response = await request.get(endpoint);
+        const responseBody = await response.json();
 
         expect(response.status()).toBe(200);
 
-        const responseBody = await response.json();
         expect(responseBody.id).not.toBeNull();
         expect(responseBody.id).not.toBe('');
 
@@ -30,21 +31,22 @@ test.describe('API Tests - BBC Sport Standings', () => {
         expect(participants.length).toBe(6);
     });
 
-    test('Verify API returns data for a different competition', async ({ request }) => {
+    test('GET request returns data for a different competition', async ({ request }) => {
 
-        const response = await request.get('/wc-poll-data/container/sport-standings?urn=urn:bbc:sportsdata:rugby-union:tournament:world-cup');
+        const response = await request.get(partialEndpoint + 'world-cup');
         const responseBody = await response.json();
-        expect(responseBody.tournaments?.[0]?.name).toContain('World Cup');
 
+        expect(responseBody).not.toBeNull();
+        expect(responseBody.tournaments?.[0]?.name).toContain('World Cup');
     });
 
-    test('Verify returning 404 for a incorrect competition', async ({ request }) => {
-        const response = await request.get('/wc-poll-data/container/sport-standings?urn=urn:bbc:sportsdata:rugby-union:tournament:incorrect-competition');
+    test('GET request returns 404 for a incorrect competition', async ({ request }) => {
+        const response = await request.get(partialEndpoint + 'incorrect-competition');
         expect(response.status()).toBe(404);
     });
 
-    test('Verify x-test-harness header is set correctly in the response', async ({ request }) => {
-        const response = await request.get('/wc-poll-data/container/sport-standings?urn=urn:bbc:sportsdata:rugby-union:tournament:world-cup', {
+    test('GET request x-test-harness header is set correctly in the response and returns correctly', async ({ request }) => {
+        const response = await request.get(endpoint, {
             headers: {
                 'x-test-harness': 'true',
             },
@@ -65,15 +67,15 @@ test.describe('API Tests - BBC Sport Standings', () => {
             'access-control-allow-origin': '*',
             'belfrage-cache-status': 'MISS',
             'bid': 'cedric',
-            'bsig': '90f8f3046fe7ee258a64cbcf9c702b1b',
+            'bsig': '367f914f4833f80ebaafdfb1246eb30e',
             'cache-control': 'public, stale-if-error=5, stale-while-revalidate=5, max-age=5',
             'connection': 'keep-alive',
             'content-encoding': 'gzip',
-            'content-length': '1436',
+            'content-length': '663',
             'content-security-policy': expect.stringContaining('default-src'), 
             'content-type': 'application/json',
             'date': expect.stringContaining(currentDate),
-            'etag': '"e4a044fbaccd42306b1f18a7cb5777ab0bcfdffd"',
+            'etag': '"d9cf69cc4cc9918f407280067301ff79a464df1a"',
             'feature-policy': expect.stringContaining('accelerometer'), 
             'nel': '{"report_to":"default","max_age":2592000,"include_subdomains":true}',
             'permissions-policy': expect.stringContaining('accelerometer'),
